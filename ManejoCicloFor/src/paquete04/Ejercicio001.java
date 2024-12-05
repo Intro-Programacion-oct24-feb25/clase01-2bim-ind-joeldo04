c93ec93f311cf0883fc6659b77932f208db92abe
Joeldo04 <joeladominguez04@gmail.com>
2024-12-05 12:00:08 -0500

    for

---
+++ ManejoCicloFor/src/paquete04/Ejercicio001.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete04;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio001 {
+        public static void main(String[] args) {
+        
+            int a = 30;
+            int b;
+            int c;
+            int d;
+            for (a = 30; a>=10; a--){
+                b = a *2;
+                c = a*3;
+                d= a*4;
+                System.out.printf("%d-%d-%d-%d \n", a, b , c,d);
+            }
+        }
+}
---
+++ ManejoCicloFor/src/paquete04/Ejercicio002.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete04;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio002 {
+     public static void main(String[] args) {
+        double fahrenheit;
+        double celcius;
+        int contador = 0;
+        
+            
+        for (fahrenheit = 20; fahrenheit<=100; fahrenheit +=4){
+            celcius = (5./9.)*(fahrenheit-32);
+            
+            System.out.printf("Operacion (%d) Farenheit: %.2f ===> Celcius %.2f"
+                            + "\n", contador, fahrenheit, celcius);
+            
+            contador++;
+            
+        }
+    }
+    
+    
+}
+            
+
